7935d1701cc9ea94febad776b19f620aaf2ee929
tinay <galvez.mcj@gmail.com>
2023-04-17 11:23:51 +0800

    modified flow of login

--- EmployeeManagement/src/EmployeeLogin.java
+++ EmployeeManagement/src/EmployeeLogin.java
@@ -1,10 +1,121 @@
-import javax.swing.*;
+import io.github.cdimascio.dotenv.Dotenv;
 
-public class EmployeeLogin {
+import javax.swing.*;
+import java.awt.*;
+import java.sql.*;
+
+public class EmployeeLogin extends JDialog{
     private JPanel mainPanel;
-    private JTextField textField1;
-    private JPasswordField passwordField1;
+    private JTextField emailTextField;
+    private JPasswordField passwordField;
     private JLabel logo;
-    private JButton LOGINButton;
-    private JButton CANCELButton;
+    private JButton loginButton;
+    private JButton cancelButton;
+
+    public User user;
+
+    public EmployeeLogin(JFrame parent) {
+        super(parent);
+        setTitle("Employee Login Form");
+        setContentPane(mainPanel);
+        setMinimumSize(new Dimension(480, 300));
+        setModal(true);
+        setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        // initialize logo
+        ImageIcon logoIcon = new ImageIcon("images/login.png");
+        logo.setIcon(logoIcon);
+
+        loginButton.addActionListener(e -> loginEmployee());
+        cancelButton.addActionListener(e -> dispose());
+
+        setVisible(true);
+    }
+
+    private void loginEmployee() {
+        String email = emailTextField.getText();
+        String password = String.valueOf(passwordField.getPassword());
+
+        // check if all fields filled out
+        if ( email.isEmpty() || password.isEmpty() ) {
+            JOptionPane.showMessageDialog(this,
+                    "Please fill out all the fields.",
+                    "Try Again",
+                    JOptionPane.WARNING_MESSAGE);
+            return;
+        }
+
+        // check in db if credentials exists
+        user = retrieveFromDB(email, password);
+
+        // more checks if user is authenticated
+        if ( user != null ) {
+            JOptionPane.showMessageDialog(this,
+                    String.format("Welcome back, %s %s", user.getFirstName(), user.getLastName()),
+                    "Login Successful",
+                    JOptionPane.INFORMATION_MESSAGE);
+            dispose();
+        } else {
+            JOptionPane.showMessageDialog(this,
+                    "Wrong credentials. Please try again.",
+                    "Try Again",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private User retrieveFromDB(String email, String password) {
+        User user = null;
+
+        // connect to db
+        // retrieve data from dot-env
+        Dotenv dotenv = null;
+        dotenv = Dotenv.configure().load();
+        final String DB_URL = dotenv.get("DB_URL");
+        final String USERNAME = dotenv.get("USER_NAME");
+        final String PASSWORD = dotenv.get("PASSWORD");
+
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+            // query db
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM employees WHERE email=? AND password=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+
+            // execute the query
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                user = new User();
+                user.setLastName(resultSet.getString("lastname"));
+                user.setFirstName(resultSet.getString("firstname"));
+                user.setPhone(resultSet.getString("phone"));
+                user.setEmail(resultSet.getString("email"));
+                user.setPassword(resultSet.getString("password"));
+            }
+
+            // close connection
+            statement.close();
+            connection.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    public static void main(String[] args) {
+        EmployeeLogin loginForm = new EmployeeLogin(null);
+        User user = loginForm.user;
+        if ( user == null ) {
+            JOptionPane.showMessageDialog(loginForm,
+                    "Login Cancelled.",
+                    "Cancelled",
+                    JOptionPane.INFORMATION_MESSAGE);
+        }
+        loginForm.dispose();
+    }
 }
